badaf726644c1d204595e03063be87788d92ae20
Jay Hung <jay@jayhung.com>
2014-07-17 21:46:12 -0700

    added console message (not output in prod) + comments

--- angular-seo.js
+++ angular-seo.js
@@ -4,10 +4,13 @@
             .run([
                 '$rootScope',
                 function($rootScope) {
+                	// mark page rendering as completed in phantomjs
+                	// if not called, phantomjs will timeout based on what's configured in angular-seo-server.js
                     $rootScope.htmlReady = function() {
+                    	console.log('$rootScope.htmlReady() fired!!');
                         $rootScope.$evalAsync(function() { // fire after $digest
                             setTimeout(function() { // fire after DOM rendering
-                                if (typeof window.callPhantom == 'function') { 
+                                if (typeof window.callPhantom == 'function') {
                                     window.callPhantom();
                                 }
                             }, 0);
