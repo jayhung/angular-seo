130addeb1f111a9fc422a4668f258d3e5fb5b76d
Jay Hung <jay@jayhung.com>
2014-07-17 21:45:24 -0700

    primarily modified route replace to prevent loop, added custom header, increased timeout, added some console logging

--- angular-seo-server.js
+++ angular-seo-server.js
@@ -13,36 +13,51 @@ var server = require('webserver').create();
 var port = parseInt(system.args[1]);
 
 var renderHtml = function(url, cb) {
-    var page = require('webpage').create();
-    page.settings.loadImages = false;
+	console.log('calling renderHtml():', url);
+
+	var page = require('webpage').create();
+	// page.isCallbackFired = false;
+	page.settings.loadImages = false;
     page.settings.localToRemoteUrlAccessEnabled = true;
-    
+	page.customHeaders = {
+		"X-Prerenderer": "phantomjs"
+	};
+
+	// page.settings.resourceTimeout = 5000; // 5 seconds
+	page.settings.resourceTimeout = 10000; // 10 seconds
+	page.onResourceTimeout = function(e) {
+		console.log(e.errorCode + ' ' + e.errorString + ': ' + e.url + "\n");	// it'll probably be 408 + 'Network timeout on resource' + {url whose request timed out}
+		page.close();
+	};
+
     page.onCallback = function() {
         cb(page.content);
-        page.close();
+    	// if ( ! page.isCallbackFired) {
+	    	// page.isCallbackFired = true;
+	        page.close();
+    	// }
     };
-    
+
     // page.onConsoleMessage = function(msg, lineNum, sourceId) {
     //    console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
     // };
-    
+
     page.onInitialized = function() {
        page.evaluate(function() {
             setTimeout(function() {
                 window.callPhantom();
-            }, 10000);
+            }, 5000);	// how long to wait in ms before marking page rendering as completed (page can call window.callPhantom() to specify completion earlier)
         });
     };
-    
+
     page.open(url);
 };
 
 server.listen(port, function (request, response) {
     var host = request.headers.Host;
     var urlPrefix = (typeof system.args[2] == 'undefined') ? 'http://' + host : system.args[2];
-    var route = request.url.replace("?_escaped_fragment_=","#");
+    var route = request.url.replace("_escaped_fragment_","_prerender_");	// rename param '_escaped_fragment_' to prevent getting caught in a loop w/ httpd daemon
     var url = urlPrefix + decodeURIComponent(route);
-    
 
     renderHtml(url , function(html) {
         response.statusCode = 200;
